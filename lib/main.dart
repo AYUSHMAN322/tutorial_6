fec982a8b8d9b7825181be815382f7635db7c0da
Rahul Dangi <ayushman.2830@gmail.com>
2020-05-08 19:28:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'dart:async';
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(
+      seconds: 2
+    ),
+            (){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Home(),),);
+        }
+    );
+  }
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlutterLogo(
+        size: 400,
+      ),
+    );
+  }
+}
+
